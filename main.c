80770e9d0970de1931b2e69de82dc7868c725e23
Clement <clement.galvez.sio@gmail.com>
2022-12-07 16:16:57 +0100

    add main & "hello develop"

---
+++ main.c
@@ -0,0 +1,12 @@
+//
+// Created by Galvez Clément on 07/12/2022.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    printf("Hello develop!");
+    return 0;
+}
